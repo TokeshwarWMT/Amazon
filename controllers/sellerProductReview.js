b21d909c805de8e9449dc032dd3bf7f760a2e791
tokeshwar <tokeshwar007@gmail.com>
2022-10-05 18:20:58 +0530

    done

--- controllers/sellerProductReview.js
+++ controllers/sellerProductReview.js
@@ -14,3 +14,23 @@ exports.create_Review = async (req, res) => {
         return res.status(500).send(e.message)
     }
 };
+
+
+exports.delete_Review = async (req, res) => {
+    let data = req.body;
+    const { productId } = req.params;
+    const { reviewId } = req.params;
+
+    data.productId = productId;
+
+    try {
+        const review = await sProductReview.findByIdAndUpdate(reviewId, {$set: data}, {new: true});
+        if (!review) {
+            return res.status(400).send('review already deleted!!')
+        }
+        await sProduct.findByIdAndUpdate(productId, { $inc: { reviews: -1 }});
+        return res.status(201).send('successfully deleted review!!')
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
--- routes/review.js
+++ routes/review.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const sellerProductReview = require('../controllers/sellerProductReview');
 
 router.post('/create_Review/:productId', sellerProductReview.create_Review);
+router.delete('/delete_Review/:productId/:reviewId', sellerProductReview.delete_Review);
 
 module.exports = router;
